24f435483982346b6e146ee68d438a508f7b9cef
Varun Urs M S <varunurs37@gmail.com>
2023-07-02 00:57:17 +0530

    resolved conflicts

--- frontend/canasu/src/App.js
+++ frontend/canasu/src/App.js
@@ -1,51 +1,42 @@
 // import logo from './logo.svg';
 import "./App.css";
 
-import React, { useEffect, useState } from 'react'
-import{
+import React, { useEffect, useState } from "react";
+import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from "react-router-dom";
-import AdminWrapper from './wrapper/AdminWrapper';
+import AdminWrapper from "./wrapper/AdminWrapper";
 import LandingPage from "./components/LandingPage";
 import Tabs from "./components/Tabs";
 import Admin from "./admin/admin";
 import { serviceGet } from "./utils/api";
 import ProtectedRoute from "./wrapper/ProtectedRoute";
+import MentorDashboard from "./mentor/MentorDashboard";
 
 function App() {
- 
-  
-
   return (
     <div className="App">
       <Router>
-<<<<<<< HEAD
         <Routes>
-          <Route
-            path="/login"
-            element={<Login updateParentState={handleUpdate} />}
-          />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Tabs />} />
           <Route path="/mentor-dashboard" element={<MentorDashboard />} />
-=======
-      
-      <Routes>
-            
-            <Route path="/" element={<LandingPage  />}/>
-            <Route path="/login" element={<Tabs  />}/>
-            <Route path="/admin" element={<ProtectedRoute><AdminWrapper /></ProtectedRoute>}>
-              <Route index element={<Navigate to="/admin/batches"/>}/>
-                <Route path="batches" element={<h1>blah</h1>}/>
-              </Route>
-            {/* <Route path="/batches" element={<Batches />}/> */}
-             
 
-            
-            
->>>>>>> 2c1f4264636db091d97ea469b96a4248e60f18c5
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminWrapper />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate to="/admin/batches" />} />
+            <Route path="batches" element={<h1>blah</h1>} />
+          </Route>
+          {/* <Route path="/batches" element={<Batches />}/> */}
         </Routes>
       </Router>
     </div>
