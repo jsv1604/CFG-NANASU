0d1d9ee5bab720564ed35d2fd6a3afdb0644a20b
Varun Urs M S <varunurs37@gmail.com>
2023-07-01 22:49:49 +0530

    Signup page done and started with mentor page

--- frontend/canasu/src/App.js
+++ frontend/canasu/src/App.js
@@ -1,37 +1,30 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import React, { useState } from 'react'
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Login from './components/Login';
-import SignUp from './mentor/SignUp';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./mentor/SignUp";
+import MentorDashboard from "./mentor/MentorDashboard";
 function App() {
-
-  const [user,setLoginUser] = useState()
+  const [user, setLoginUser] = useState();
 
   const handleUpdate = (newValue) => {
     setLoginUser(newValue);
   };
 
-
   return (
     <div className="App">
       <Router>
         HOME
-      
-      <Routes>
-            
-            <Route path="/login" element={<Login  updateParentState={handleUpdate}/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-            
-            
-            
+        <Routes>
+          <Route
+            path="/login"
+            element={<Login updateParentState={handleUpdate} />}
+          />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mentor-dashboard" element={<MentorDashboard />} />
         </Routes>
-      
       </Router>
     </div>
   );
---
+++ frontend/canasu/src/mentor/MentorDashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/MentorDashboard.css";
+
+const MentorDashboard = () => {
+  return (
+    <>
+      <div className="mentorBody">
+        <div className="leftsection"></div>
+        <div className="styles.centralBody"></div>
+      </div>
+    </>
+  );
+};
+
+export default MentorDashboard;
