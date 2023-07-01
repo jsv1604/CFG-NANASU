2c617e2a0f38de3a31ed0ab3c93b989b57d3b21c
Jeffrey S Varghese <jeff0716042@gmail.com>
2023-07-01 20:59:21 +0530

    nav

---
+++ frontend/canasu/src/components/CSVUploader.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const CSVUploader = () => {
+  const [csvData, setCSVData] = useState([]);
+  
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    
+    reader.onload = (e) => {
+      const contents = e.target.result;
+      const rows = contents.split('\n');
+      
+      // Parsing CSV data
+      const data = [];
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i].split(',');
+        data.push(row);
+      }
+      
+      setCSVData(data);
+    };
+    
+    reader.readAsText(file);
+  };
+  
+  return (
+    <div>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      
+      {csvData.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {csvData[0].map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {csvData.slice(1).map((row, index) => (
+              <tr key={index}>
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default CSVUploader;
