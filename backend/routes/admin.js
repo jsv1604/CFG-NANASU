aedb20abd6b0c1ca358591dc8fc56d0f92f81906
Varun Urs M S <varunurs37@gmail.com>
2023-07-02 06:59:35 +0530

    Admin Module - Batches Table, Admin ViewPage Completed

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -6,224 +6,264 @@ const BatchModel = require("../database/schema/Batch");
 const ses = require("../services/aws");
 const Module = require("../database/schema/Module");
 const AdminModel = require("../database/schema/admin");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const Router = express.Router();
 
-//add mentees 
+//add mentees
 
-Router.post('/add/mentee', async (req, res) => {
-    try {
-        const { students } = req.body;
-        for (i in students) {
-            const { email, name, language } = students[i];
-            var student = await MenteeModel.findOne({ email });
-            const mentor = await MentorModel.findOne({ language, status: 'available' });
-            if (!student) {
-                student = await MenteeModel.create({
-                    name,
-                    email,
-                    password: "random",
-                    language
-                });
-            }
-            await MentorModel.findByIdAndUpdate(mentor._id, {
-                $set: {
-                    status: "assigned"
-                }
-            })
-            const batch = await BatchModel.create({
-                mentor: mentor._id,
-                mentee: student._id
-            })
-            const token = jwt.sign({ id: student._id.toString(), email: student.email }, 'forget-pass', { expiresIn: "10m" });
+Router.post("/add/mentee", async (req, res) => {
+  try {
+    const { students } = req.body;
+    for (i in students) {
+      const { email, name, language } = students[i];
+      var student = await MenteeModel.findOne({ email });
+      const mentor = await MentorModel.findOne({
+        language,
+        status: "available",
+      });
+      if (!student) {
+        student = await MenteeModel.create({
+          name,
+          email,
+          password: "random",
+          language,
+        });
+      }
+      await MentorModel.findByIdAndUpdate(mentor._id, {
+        $set: {
+          status: "assigned",
+        },
+      });
+      const batch = await BatchModel.create({
+        mentor: mentor._id,
+        mentee: student._id,
+      });
+      const token = jwt.sign(
+        { id: student._id.toString(), email: student.email },
+        "forget-pass",
+        { expiresIn: "10m" }
+      );
 
-            await ses.sendEmail({
-                Destination: {
-                    ToAddresses: [email],
-                },
-                Message: {
-                    Body: {
-                        Html: {
-                            Data: `forget pass link : localhost:3000/auth/reset/${token}`
-                        },
-                    },
-                    Subject: {
-                        Charset: 'UTF-8',
-                        Data: 'Reset password link',
-                    },
-                },
-                ReplyToAddresses: [],
-                Source: 'samarth <samarthsingh890.ss@gmail.com>',
-            }, function (err, data) {
-                if (err) {
-                    console.log(err);
-                    throw new Error(err);
-                } else console.log(data);
-            });
+      await ses.sendEmail(
+        {
+          Destination: {
+            ToAddresses: [email],
+          },
+          Message: {
+            Body: {
+              Html: {
+                Data: `forget pass link : localhost:3000/auth/reset/${token}`,
+              },
+            },
+            Subject: {
+              Charset: "UTF-8",
+              Data: "Reset password link",
+            },
+          },
+          ReplyToAddresses: [],
+          Source: "samarth <samarthsingh890.ss@gmail.com>",
+        },
+        function (err, data) {
+          if (err) {
+            console.log(err);
+            throw new Error(err);
+          } else console.log(data);
         }
-        return res.status(200).json({message: "added students successfully", success: true});
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: error.message, success: false });
+      );
     }
+    return res
+      .status(200)
+      .json({ message: "added students successfully", success: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-
-Router.get('/mentor', async (req, res) => {
-    try {
-        const mentors = await MentorModel.find({});
-        return res.statusCode(200).json({ message: "Fetched all mentore", mentors, success });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-    }
+Router.get("/mentor", async (req, res) => {
+  try {
+    const mentors = await MentorModel.find({});
+    return res
+      .statusCode(200)
+      .json({ message: "Fetched all mentore", mentors, success });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
-Router.put('/mentor/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const mentor = await MentorModel.findByIdAndUpdate(id,{
-            $set:{
-                status:"available"
-            }
-        });
-        return res.statusCode(200).json({ message: "updated all mentor", mentor, success });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-    }
+Router.put("/mentor/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const mentor = await MentorModel.findByIdAndUpdate(id, {
+      $set: {
+        status: "available",
+      },
+    });
+    return res
+      .statusCode(200)
+      .json({ message: "updated all mentor", mentor, success });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
-Router.put('/batch/mentor', async (req, res) => {
-    try {
-        const { batchId, mentorId } = req.query;
-        const batch = await BatchModel.findByIdAndUpdate(batchId, {
-            $set: {
-                mentor: mentorId
-            }
-        });
-        return res.statusCode(200).json({ success: true, batch });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+Router.put("/batch/mentor", async (req, res) => {
+  try {
+    const { batchId, mentorId } = req.query;
+    const batch = await BatchModel.findByIdAndUpdate(batchId, {
+      $set: {
+        mentor: mentorId,
+      },
+    });
+    return res.statusCode(200).json({ success: true, batch });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
-Router.post('/module', async (req, res) => {
-    try {
-        const newModule = await Module.create({ ...req.body });
+Router.post("/module", async (req, res) => {
+  try {
+    const newModule = await Module.create({ ...req.body });
 
-
-        return res.status(200).json({ token, module: newModule, success: true, message: "Module created Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        module: newModule,
+        success: true,
+        message: "Module created Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
-Router.delete('/module/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const del = await Module.findByIdAndDelete(id);
-        return res.statusCode(200).json({ success: true, message: "Module deleted Successfully" })
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-    }
-});
-
-Router.post('/add-admin', async (req, res) => {
-    try {
-        const newAdmin = await Admin.create({ ...req.body });
-
-
-        return res.status(200).json({ token, admin: newAdmin, success: true, message: "Admin added Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+Router.delete("/module/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const del = await Module.findByIdAndDelete(id);
+    return res
+      .statusCode(200)
+      .json({ success: true, message: "Module deleted Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.delete('/add-admin/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const del = await Admin.findByIdAndDelete(id);
-        return res.statusCode(200).json({ success: true, message: "Admin deleted Successfully" })
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-    }
+Router.post("/add-admin", async (req, res) => {
+  try {
+    const newAdmin = await Admin.create({ ...req.body });
+
+    return res
+      .status(200)
+      .json({
+        token,
+        admin: newAdmin,
+        success: true,
+        message: "Admin added Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-
-Router.get('/batch', async (req, res) => {
-    try {
-        const batches = await BatchModel.find({}).populate('mentor mentee');
-
-
-        return res.status(200).json({ batches, success: true, message: "Batches fetched Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+Router.delete("/add-admin/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const del = await Admin.findByIdAndDelete(id);
+    return res
+      .statusCode(200)
+      .json({ success: true, message: "Admin deleted Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.get('/batch/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        // const newBatch = await BatchModel.findById(id).populate("modules");
-        const [batch] = await BatchModel.aggregate([
+Router.get("/batch", async (req, res) => {
+  try {
+    const batches = await BatchModel.find({}).populate("mentor mentee");
+
+    return res
+      .status(200)
+      .json({
+        batches,
+        success: true,
+        message: "Batches fetched Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
+});
+
+Router.get("/batch/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const newBatch = await BatchModel.findById(id).populate("modules");
+    const [batch] = await BatchModel.aggregate([
+      {
+        $match: {
+          $expr: {
+            $eq: ["$_id", toObjectId(id)],
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "modules",
+          let: { modules: "$modules" },
+          pipeline: [
             {
-                $match: {
-                    $expr: {
-                        $eq: ['$_id', toObjectId(id)],
-                    },
+              $match: {
+                $expr: {
+                  $eq: ["$_id", "$$modules"],
                 },
+              },
             },
             {
-                $lookup: {
-                    from: "modules",
-                    let: { modules: '$modules' },
-                    pipeline: [
-                        {
-                            $match: {
-                                $expr: {
-                                    $eq: ['$_id', '$$modules'],
-                                },
-                            },
-                        },
-                        {
-                            $lookup: {
-                                from: "sessions",
-                                let: { sessions: '$sessions' },
-                                pipeline: [
-                                    {
-                                        $match: {
-                                            $expr: {
-                                                $eq: ['$_id', '$$sessions'],
-                                            },
-                                        },
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            }
-        ]);
+              $lookup: {
+                from: "sessions",
+                let: { sessions: "$sessions" },
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $eq: ["$_id", "$$sessions"],
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ]);
 
-        return res.status(200).json({ token, batch, success: true, message: "Batch fetched Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        batch,
+        success: true,
+        message: "Batch fetched Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.get('/feedback/:batchId', async (req, res) => {
-    try {
-        const { batchId } = req.params;
-        const newFeedback = await FeedbackModel.findById(batchId);
+Router.get("/feedback/:batchId", async (req, res) => {
+  try {
+    const { batchId } = req.params;
+    const newFeedback = await FeedbackModel.findById(batchId);
 
-        return res.status(200).json({ token, admin: newAdmin, success: true, message: "Feedback received Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        admin: newAdmin,
+        success: true,
+        message: "Feedback received Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-
-
-
-module.exports = Router; 
+module.exports = Router;
--- backend/routes/mentor.js
+++ backend/routes/mentor.js
@@ -5,167 +5,210 @@ const MenteeModel = require("../database/schema/Mentee");
 const BatchModel = require("../database/schema/Batch");
 const Router = express.Router();
 
-Router.post('/module', async (req, res) => {
-    try {
-        const newModule = await Module.create({ ...req.body });
+Router.post("/module", async (req, res) => {
+  try {
+    const newModule = await Module.create({ ...req.body });
 
-
-        return res.status(200).json({ token, module: newModule, success: true, message: "Module created Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        module: newModule,
+        success: true,
+        message: "Module created Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
-Router.delete('/module/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const del = await Module.findByIdAndDelete(id);
-        return res.statusCode(200).json({ success: true, message: "Module deleted Successfully" })
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-    }
-});
-
-Router.post('/session/:moduleId', async (req, res) => {
-    try {
-        const {moduleId} = req.params;
-        const newSession = await SessionsModel.create({ ...req.body });
-        await Module.findByIdAndUpdate(moduleId,{
-           $push:{
-            session:newSession._id
-           }
-        },{
-            new: true,
-            upsert: true
-        })
-
-        return res.status(200).json({ token, session: newSession, success: true, message: "Session created Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
-});
-Router.delete('/session/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const del = await SessionsModel.findByIdAndDelete(id);
-        return res.statusCode(200).json({ success: true, message: "Session deleted Successfully" })
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-    }
+Router.delete("/module/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const del = await Module.findByIdAndDelete(id);
+    return res
+      .statusCode(200)
+      .json({ success: true, message: "Module deleted Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.get('/batch/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        // const batch = await BatchModel.findById(id).populate("Modules");
-        const [batch] = await BatchModel.aggregate([
+Router.post("/session/:moduleId", async (req, res) => {
+  try {
+    const { moduleId } = req.params;
+    const newSession = await SessionsModel.create({ ...req.body });
+    await Module.findByIdAndUpdate(
+      moduleId,
+      {
+        $push: {
+          session: newSession._id,
+        },
+      },
+      {
+        new: true,
+        upsert: true,
+      }
+    );
+
+    return res
+      .status(200)
+      .json({
+        token,
+        session: newSession,
+        success: true,
+        message: "Session created Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
+});
+Router.delete("/session/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const del = await SessionsModel.findByIdAndDelete(id);
+    return res
+      .statusCode(200)
+      .json({ success: true, message: "Session deleted Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
+});
+
+Router.get("/batch/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const batch = await BatchModel.findById(id).populate("Modules");
+    const [batch] = await BatchModel.aggregate([
+      {
+        $match: {
+          $expr: {
+            $eq: ["$_id", toObjectId(id)],
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "modules",
+          let: { modules: "$modules" },
+          pipeline: [
             {
-                $match: {
-                    $expr: {
-                        $eq: ['$_id', toObjectId(id)],
-                    },
+              $match: {
+                $expr: {
+                  $eq: ["$_id", "$$modules"],
                 },
+              },
             },
             {
-                $lookup: {
-                    from: "modules",
-                    let: { modules: '$modules' },
-                    pipeline: [
-                        {
-                            $match: {
-                                $expr: {
-                                    $eq: ['$_id', '$$modules'],
-                                },
-                            },
-                        },
-                        {
-                            $lookup: {
-                                from: "sessions",
-                                let: { sessions: '$sessions' },
-                                pipeline: [
-                                    {
-                                        $match: {
-                                            $expr: {
-                                                $eq: ['$_id', '$$sessions'],
-                                            },
-                                        },
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            }
-        ]);
+              $lookup: {
+                from: "sessions",
+                let: { sessions: "$sessions" },
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $eq: ["$_id", "$$sessions"],
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ]);
 
-
-        return res.status(200).json({ token, batch: batch, success: true, message: "Batch fetched Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        batch: batch,
+        success: true,
+        message: "Batch fetched Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.get('/batch/:mentorId', async (req, res) => {
-    try {
-        const { mentorId } = req.params
-        const newBatch = await BatchModel.find({ mentor: mentorId }).populate("modules");
+Router.get("/batch/:mentorId", async (req, res) => {
+  try {
+    const { mentorId } = req.params;
+    const newBatch = await BatchModel.find({ mentor: mentorId }).populate(
+      "modules"
+    );
 
-
-        return res.status(200).json({ token, admin: newAdmin, success: true, message: "Batch fetched Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        admin: newAdmin,
+        success: true,
+        message: "Batch fetched Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.get('/mentee/:menteeId', async (req, res) => {
-    try {
-        const { mentorId } = req.params
-        const Mentee = await MenteeModel.findById(mentorId);
+Router.get("/mentee/:menteeId", async (req, res) => {
+  try {
+    const { mentorId } = req.params;
+    const Mentee = await MenteeModel.findById(mentorId);
 
-
-        return res.status(200).json({ token, admin: newAdmin, success: true, message: "Mentee fetched Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        admin: newAdmin,
+        success: true,
+        message: "Mentee fetched Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
-Router.put('/progress/:moduleId', async (req, res) => {
-    try {
-        const { moduleId } = req.params
-        const module = await Module.findByIdAndUpdate(moduleId, {
-            $set: {
-                progress: 1
-            }
-        }, {
-            new: true,
-            upsert: true
-        });
+Router.put("/progress/:moduleId", async (req, res) => {
+  try {
+    const { moduleId } = req.params;
+    const module = await Module.findByIdAndUpdate(
+      moduleId,
+      {
+        $set: {
+          progress: 1,
+        },
+      },
+      {
+        new: true,
+        upsert: true,
+      }
+    );
 
-
-        return res.status(200).json({ token, module: module, success: true, message: "modules fetched Successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
-
-    }
+    return res
+      .status(200)
+      .json({
+        token,
+        module: module,
+        success: true,
+        message: "modules fetched Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message, success: false });
+  }
 });
 
 //to do upcoming sessions
 
 //mentee APIs
 
-Router.get('/upcoming', async (req,res)=>{
-    try {
-        return res.json({});
-    } catch (error) {
-        console.log(error);
-        return res.status(500)
-    }
+Router.get("/upcoming", async (req, res) => {
+  try {
+    return res.json({});
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
 });
-
-
 
 module.exports = Router;
